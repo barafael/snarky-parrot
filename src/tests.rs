87d4362e669ea9e3dc1f9762ebf9386f93e4c7ad
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2020-11-16 09:45:28 +0100

    Update tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -3,11 +3,6 @@ extern crate test;
 use super::*;
 use self::test::Bencher;
 
-#[test]
-fn it_works() {
-    assert_eq!(4, 2+2);
-}
-
 #[bench]
 fn bench_short_alice(b: &mut Bencher) {
     let data = read_data("alice_oz.txt");
