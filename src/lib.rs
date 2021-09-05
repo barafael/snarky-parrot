63d4ea62e12053bff5ba84610b074a52130e1445
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-09-05 16:18:46 +0200

    Add rule generation with slice::from_raw_parts

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use core::slice;
 use error::Error;
 use rand::{seq::SliceRandom, thread_rng, Rng};
 use std::collections::HashMap;
@@ -21,14 +22,34 @@ pub fn generate_rule_from_data(content: &str, key_size: usize) -> Result<MarkovC
     for slice in words.windows(key_size + 1) {
         let (key, value) = slice.split_at(key_size);
         let value = value[0];
-        match dict.get_mut(key) {
-            Some(e) => {
-                e.push(value);
-            }
-            None => {
-                dict.insert(key.to_vec(), vec![value]);
-            }
-        }
+        dict.entry(key.to_vec()).or_default().push(value);
+    }
+
+    Ok(dict)
+}
+
+pub fn generate_rule_from_data_in_place(
+    content: &str,
+    key_size: usize,
+) -> Result<HashMap<&str, Vec<&str>>, Error> {
+    if key_size < 1 {
+        return Err(Error::InvalidKeySize);
+    }
+
+    let words: Vec<&str> = content.split_whitespace().collect();
+
+    let mut dict: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    for slice in words.windows(key_size + 1) {
+        let (key, value) = slice.split_at(key_size);
+        let value = value[0];
+
+        let ptr = key[0].as_ptr();
+        let last = key.last().unwrap();
+        let len = (last.as_ptr() as usize + last.len()) - ptr as usize;
+        let slice = unsafe { slice::from_raw_parts(ptr, len) };
+        let key = unsafe { std::str::from_utf8_unchecked(slice) };
+        dict.entry(key).or_default().push(value);
     }
 
     Ok(dict)
--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::generate_rule_from_data;
+use crate::{generate_rule_from_data, generate_rule_from_data_in_place};
 
 #[test]
 fn generate_rule() {
@@ -11,6 +11,20 @@ fn generate_rule() {
     expected.insert(vec!["ipsum", "dolor"], vec!["et"]);
     expected.insert(vec!["dolor", "et"], vec!["ami"]);
     expected.insert(vec!["et", "ami"], vec!["coloram"]);
+
+    assert_eq!(rule, expected);
+}
+
+#[test]
+fn generate_rule_in_place() {
+    let content = "Lorem ipsum   dolor  et ami coloram";
+    let rule = generate_rule_from_data_in_place(content, 2).unwrap();
+
+    let mut expected = HashMap::new();
+    expected.insert("Lorem ipsum", vec!["dolor"]);
+    expected.insert("ipsum   dolor", vec!["et"]);
+    expected.insert("dolor  et", vec!["ami"]);
+    expected.insert("et ami", vec!["coloram"]);
 
     assert_eq!(rule, expected);
 }
