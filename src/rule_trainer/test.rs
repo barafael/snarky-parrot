d4b307dbd11df1c26755522ab5128d3ccac1605b
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-09-06 23:07:55 +0200

    Add compare test

--- src/rule_trainer/test.rs
+++ src/rule_trainer/test.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 
-use crate::rule_trainer::{generate_rule_from_data, generate_rule_from_data_unsafe};
+use crate::rule_trainer::{
+    generate_rule_from_data, generate_rule_from_data_unsafe, generate_rule_from_data_vec,
+};
 
 #[test]
 fn generate_rule() {
@@ -41,4 +43,21 @@ fn generate_rule_in_place_safe() {
     expected.insert("et ami", vec!["coloram"]);
 
     assert_eq!(rule, expected);
+}
+
+#[test]
+fn generate_rule_in_place_with_spaces() {
+    let content = "Lorem ipsum   dolor  et ami coloram";
+    let rule = generate_rule_from_data(content, 2).unwrap();
+    let rule_unsafe = generate_rule_from_data_unsafe(content, 2).unwrap();
+
+    assert_eq!(rule, rule_unsafe);
+
+    let rule_vec = generate_rule_from_data_vec(content, 2).unwrap();
+    let rule: HashMap<Vec<&str>, Vec<&str>> = rule
+        .iter()
+        .map(|(k, v)| (k.split_whitespace().collect(), v.clone()))
+        .collect();
+
+    assert_eq!(rule, rule_vec);
 }
